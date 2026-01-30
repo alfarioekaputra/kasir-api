f5666363091d836831dbb57ffc1c430418cf8e48
Rio <alfarioekaputra@gmail.com>
2026-01-30 21:49:44 +0700

    Extract routing to router package

--- main.go
+++ main.go
@@ -11,9 +11,7 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/spf13/viper"
 	"labkoding.my.id/kasir-api/database"
-	"labkoding.my.id/kasir-api/handler"
-	"labkoding.my.id/kasir-api/repositories"
-	"labkoding.my.id/kasir-api/services"
+	"labkoding.my.id/kasir-api/router"
 )
 
 type Config struct {
@@ -41,55 +39,15 @@ func main() {
 	}
 	defer db.Close()
 
-	categoryRepo := repositories.NewCategoryRepository(db)
-	categoryService := services.NewCategoryService(categoryRepo)
-	categoryHandler := handler.NewCategoryHandler(categoryService)
-
-	productRepo := repositories.NewProductRepository(db)
-	productService := services.NewProductService(productRepo)
-	productHandler := handler.NewProductHandler(productService)
-
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
 
-	r.Route("/categories", func(r chi.Router) {
-		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-			categoryHandler.GetAllCategory(w, r)
-		})
-		r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-			categoryHandler.CreateCategory(w, r)
-		})
-		r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
-			categoryHandler.GetCategoryByID(w, r)
-		})
-		r.Put("/update/{id}", func(w http.ResponseWriter, r *http.Request) {
-			categoryHandler.UpdateCategory(w, r)
-		})
-		r.Delete("/delete/{id}", func(w http.ResponseWriter, r *http.Request) {
-			categoryHandler.DeleteCategory(w, r)
-		})
-	})
-
-	r.Route("/products", func(r chi.Router) {
-		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-			productHandler.GetAllProduct(w, r)
-		})
-		r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-			productHandler.CreateProduct(w, r)
-		})
-		r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
-			productHandler.GetProductByID(w, r)
-		})
-		r.Put("/update/{id}", func(w http.ResponseWriter, r *http.Request) {
-			productHandler.UpdateProduct(w, r)
-		})
-		r.Delete("/delete/{id}", func(w http.ResponseWriter, r *http.Request) {
-			productHandler.DeleteProduct(w, r)
-		})
-	})
+	appRouter := router.NewRouter(db, r)
+	appRouter.RegisterAllRoutes()
 
 	addr := "0.0.0.0:" + config.Port
 	fmt.Println("Server running di", addr)
---
+++ router/router.go
@@ -0,0 +1,52 @@
+package router
+
+import (
+	"database/sql"
+
+	"github.com/go-chi/chi/v5"
+	"labkoding.my.id/kasir-api/handler"
+	"labkoding.my.id/kasir-api/repositories"
+	"labkoding.my.id/kasir-api/services"
+)
+
+type Router struct {
+	db     *sql.DB
+	router chi.Router
+}
+
+func NewRouter(db *sql.DB, r chi.Router) *Router {
+	return &Router{db: db, router: r}
+}
+
+func (rt *Router) RegisterCategoryRoutes() {
+	categoryRepo := repositories.NewCategoryRepository(rt.db)
+	categoryService := services.NewCategoryService(categoryRepo)
+	categoryHandler := handler.NewCategoryHandler(categoryService)
+
+	rt.router.Route("/categories", func(r chi.Router) {
+		r.Get("/", categoryHandler.GetAllCategory)
+		r.Post("/", categoryHandler.CreateCategory)
+		r.Get("/{id}", categoryHandler.GetCategoryByID)
+		r.Put("/{id}", categoryHandler.UpdateCategory)
+		r.Delete("/{id}", categoryHandler.DeleteCategory)
+	})
+}
+
+func (rt *Router) RegisterProductRoutes() {
+	productRepo := repositories.NewProductRepository(rt.db)
+	productService := services.NewProductService(productRepo)
+	productHandler := handler.NewProductHandler(productService)
+
+	rt.router.Route("/products", func(r chi.Router) {
+		r.Get("/", productHandler.GetAllProduct)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/{id}", productHandler.GetProductByID)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+}
+
+func (rt *Router) RegisterAllRoutes() {
+	rt.RegisterCategoryRoutes()
+	rt.RegisterProductRoutes()
+}
